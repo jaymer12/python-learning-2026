b96fc90168cddb86dffcc79575794d1d5cd9a587
JAY <jaymer946@gmail.com>
2026-04-12 14:36:38 -0600

    Add Day 1 - Python basics and f-string examples

---
+++ hello.py
@@ -0,0 +1,15 @@
+print("Hello World! I'm ready to learn Python in 2026 🚀")
+
+name = "JAY"
+print(f"Hi {name}, welcome to your Python journey!") 
+
+name = "JAY"
+age = 25
+city = "Regina"
+
+print(f"Hi {name}!")
+print(f"You are {age} years old.")
+print(f"Welcome from {city}, Saskatchewan!")
+
+# You can even do simple math inside {}
+print(f"Next year you will be {age + 1} years old.")
